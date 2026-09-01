7060c4a020c3f7d259bd79d81977c3c5ead33f5b
mpmabeyrathne <pasindumaduwantha2019@gmail.com>
2026-09-01 12:02:36 +0530

    chore(PH01-001): initialize Node.js project

---
+++ src/config/constants.ts
@@ -0,0 +1,6 @@
+export const APP_CONFIG = {
+    name: 'Cymerics',
+    version: '1.0.0',
+    description:
+      'Production-grade Node.js platform for autonomous AI agents, secure execution, distributed systems, and scalable cloud infrastructure.',
+  } as const;
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import Fastify from 'fastify'
+import { APP_CONFIG } from './config/constants.js';
+import 'dotenv/config';
+
+const PORT = Number(process.env.PORT)  || 3999;
+const server = Fastify({logger: true});
+
+server.get('/', async () => {
+    return {
+        message : `${APP_CONFIG.name} backend is running`
+    }
+});
+
+const start = async () =>{
+    try{
+        server.listen({
+            port: PORT,
+            host: '0.0.0.0'
+        });
+    }catch(error){
+        server.log.error(error);
+        process.exit(1);
+    }
+ 
+};
+
+start();

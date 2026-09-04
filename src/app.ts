1273eb38f8a2e3b813ea9c2f1e57f61ea785c7ee
Pasindu Maduwantha <96675144+mpmabeyrathne@users.noreply.github.com>
2026-09-04 14:50:11 +0530

    feat(PH01-013): implement health check endpoint (#60)

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import Fastify from "fastify";
 import { APP_CONFIG, ENV_CONFIG } from "./configuration/index.js";
 import { registerErrorHandler, AppError } from "./errors/index.js";
+import { healthRoutes } from "./routes/health.routes.js";
 
 export function buildApp(){
     const app = Fastify({
@@ -18,26 +19,7 @@ export function buildApp(){
      registerErrorHandler(app);
 
     // Register routes
-    app.get('/', async () =>{
-        return {
-            message : `${APP_CONFIG.name} backend is running`
-        }
-    })
-
-    app.get('/test-error', async () => {
-        throw new AppError(
-            'TEST_ERROR',
-            400,
-            'This is a test error',
-        );
-    });
-
-    app.get('/test-request-error', async () => {
-        throw new AppError(
-            'TEST_ERROR',
-            400,
-            'Request ID test error',
-        );
-    });
+    app.register(healthRoutes);
+    
     return app;
 }
